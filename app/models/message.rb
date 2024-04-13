8d377b15913a571a88c6cf981307027d2dee6295
Boston Essary <BostonCEssary@gmail.com>
2024-04-13 13:30:38 -0700

    (BS) main: add receiver and sender foreign keys to messages table

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,15 +2,28 @@
 #
 # Table name: messages
 #
-#  id         :bigint           not null, primary key
-#  body       :text
-#  edited     :boolean
-#  read       :boolean
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id          :bigint           not null, primary key
+#  body        :text
+#  edited      :boolean
+#  read        :boolean
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  receiver_id :bigint           not null
+#  sender_id   :bigint           not null
+#
+# Indexes
+#
+#  index_messages_on_receiver_id  (receiver_id)
+#  index_messages_on_sender_id    (sender_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (receiver_id => users.id)
+#  fk_rails_...  (sender_id => users.id)
 #
 class Message < ApplicationRecord
-    belongs_to :user
+    belongs_to :receiver, class_name: "User", foreign_key: "receiver_id"
+    belongs_to :sender, class_name: "User", foreign_key: "sender_id"
     
     has_many_attached :attachments
 
--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -2,12 +2,24 @@
 #
 # Table name: messages
 #
-#  id         :bigint           not null, primary key
-#  body       :text
-#  edited     :boolean
-#  read       :boolean
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id          :bigint           not null, primary key
+#  body        :text
+#  edited      :boolean
+#  read        :boolean
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  receiver_id :bigint           not null
+#  sender_id   :bigint           not null
+#
+# Indexes
+#
+#  index_messages_on_receiver_id  (receiver_id)
+#  index_messages_on_sender_id    (sender_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (receiver_id => users.id)
+#  fk_rails_...  (sender_id => users.id)
 #
 FactoryBot.define do
   factory :message do
