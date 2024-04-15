0e88cb4dc59177e2442f9c30917fc9d30b6c23c6
Boston Essary <BostonCEssary@gmail.com>
2024-04-14 21:03:01 -0700

    (BS) main: authorize only members of the chat to see said chat

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,4 +1,6 @@
 class ChatsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :check_authorization, only: [:show]
   def index
     @chats = Chat.all
   end
@@ -12,5 +14,19 @@ class ChatsController < ApplicationController
   def show
     @chat = Chat.find(params[:id])
     @message = @chat.messages.build
+  end
+end
+
+private
+
+def authorized?
+  @chat = Chat.find(params[:id])
+  @chat.users.include?(current_user)
+end
+
+def check_authorization
+  unless authorized?
+    flash[:error] = "You are not authorized to view this chat"
+    redirect_to chats_path
   end
 end
--- app/models/chat.rb
+++ app/models/chat.rb
@@ -10,4 +10,5 @@ class Chat < ApplicationRecord
   has_many :messages
   has_many :chat_members
   has_many :users, through: :chat_members
+
 end
