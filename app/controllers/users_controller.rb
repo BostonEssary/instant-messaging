cc0d9d5ff86463da3c01421d08d4a54b8fc0c3a2
Boston <boston.essary@strongmind.com>
2024-05-06 18:44:10 -0700

    (B) main: add correct turbo frame to users#show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,24 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.all
+    if params[:query]
+      @users = User.where("username LIKE ?", params[:query] + "%")
+    else
+      @users = User.all
+    end
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html
+    end
+
   end
   def show
     @user = User.find(params[:id])
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html
+    end
   end
 end
